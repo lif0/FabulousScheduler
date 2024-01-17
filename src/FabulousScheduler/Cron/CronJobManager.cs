94027283db5228d6372e6bb73689d90a225a17f6
lif0 <22912194+lif0@users.noreply.github.com>
2024-01-17 13:04:51 +0300

    [FS-12] fix test & add trigger run unit-test when start pull request;

--- src/FabulousScheduler/Cron/CronJobManager.cs
+++ src/FabulousScheduler/Cron/CronJobManager.cs
@@ -31,18 +31,69 @@ public static class CronJobManager
         InternalInit(config);
     }
 
+    #region RegisterJob
+
     /// <summary>
     /// Register a job on jobManager
     /// </summary>
     /// <param name="action">A algorithm that should be repeated</param>
     /// <param name="sleepDuration">How long time a job will be sleep after success execute</param>
-    public static void Register(Func<Task> action, TimeSpan sleepDuration)
+    /// <returns>return JobID</returns>
+    public static Guid Register(Func<Task> action, TimeSpan sleepDuration)
     {
-        _manager.RegisterCron(action, sleepDuration);
+        return InternalRegisterJob(action, null, null, sleepDuration);
     }
+
+    /// <summary>
+    /// Register a job on jobManager
+    /// </summary>
+    /// <param name="action">A action that should be repeated</param>
+    /// <param name="name">The job's name</param>
+    /// <param name="sleepDuration">How long time a job will be sleep after success execute</param>
+    /// <returns>return JobID</returns>
+    public static Guid Register(Func<Task> action, string name, TimeSpan sleepDuration)
+    {
+        return InternalRegisterJob(action, name, null, sleepDuration);
+    }
+    
+    /// <summary>
+    /// Register a job on jobManager
+    /// </summary>
+    /// <param name="action">A action that should be repeated</param>
+    /// <param name="name">The job's name</param>
+    /// <param name="category">The job's category</param>
+    /// <param name="sleepDuration">How long time a job will be sleep after success execute</param>
+    /// <returns>return JobID</returns>
+    public static Guid Register(Func<Task> action, string name, string category, TimeSpan sleepDuration)
+    {
+        return InternalRegisterJob(action, name, category, sleepDuration);
+    }
+
+    #endregion
 
     #region Private
 
+    private static Guid InternalRegisterJob(Func<Task> action, string? name, string? category, TimeSpan sleepDuration)
+    {
+        switch (name, category)
+        {
+            case (null, null):
+            {
+                return _manager.RegisterCron(action, sleepDuration);
+            } 
+            case (not null, null):
+            {
+                return _manager.RegisterCron(action, name, sleepDuration);
+            }
+            case (not null, not null):
+            {
+                return _manager.RegisterCron(action, name, category, sleepDuration);
+            }
+            default:
+                return _manager.RegisterCron(action, sleepDuration);
+        }
+    }
+    
     private static void InternalInit(Config? config = null)
     {
         _manager = new(config ?? Config.Default);
